57ccaa148b31ab2c8235e3fbf9a7d10b11d90732
Bradley Grainger <bgrainger@gmail.com>
2010-12-29 15:28:41 -0800

    Added bspatch.

---
+++ src/bspatch/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+using System.IO;
+using BsDiff;
+
+namespace BsPatch
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// check for correct usage
+			if (args.Length != 3)
+			{
+				Console.Error.WriteLine("bsdiff oldfile newfile patchfile");
+				return;
+			}
+
+			string oldFile = args[0];
+			string patchFile = args[1];
+			string newFile = args[2];
+
+			try
+			{
+				using (FileStream input = new FileStream(oldFile, FileMode.Open, FileAccess.Read, FileShare.Read))
+				using (FileStream output = new FileStream(newFile, FileMode.Create))
+					BinaryPatchUtility.Apply(input, () => new FileStream(patchFile, FileMode.Open, FileAccess.Read, FileShare.Read), output);
+			}
+			catch (FileNotFoundException ex)
+			{
+				Console.Error.WriteLine("Could not open '{0}'.", ex.FileName);
+			}
+		}
+	}
+}
