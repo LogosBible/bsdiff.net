cb7acc2bc2d3386f589785194a26cc5ce393e0b1
Bradley Grainger <bgrainger@gmail.com>
2010-12-29 15:22:54 -0800

    Set assembly version to 4.3 (same as the C version it was copied from).

--- src/bsdiff/Properties/AssemblyInfo.cs
+++ src/bsdiff/Properties/AssemblyInfo.cs
@@ -2,11 +2,8 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("BsDiff")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Port of bsdiff to .NET; see https://github.com/LogosBible/bsdiff.net.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Logos Bible Software")]
 [assembly: AssemblyProduct("BsDiff")]
@@ -14,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("4.3.0.0")]
+[assembly: AssemblyFileVersion("4.3.0.0")]
