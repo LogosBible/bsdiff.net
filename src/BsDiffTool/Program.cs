63824f47eef8c236d6794bf42c2b17f94a6abca7
Bradley Grainger <bgrainger@gmail.com>
2023-02-10 22:51:31 -0800

    Use file-scoped namespaces and implicit usings.

--- src/BsDiffTool/Program.cs
+++ src/BsDiffTool/Program.cs
@@ -1,10 +1,7 @@
-
-using System;
 using System.Diagnostics;
-using System.IO;
-using System.Linq;
+using BsDiff;
 
-namespace BsDiff
+namespace BsDiffTool
 {
 	class Program
 	{
@@ -19,18 +16,16 @@ namespace BsDiff
 
 			// check for special command-line switch that performs a self-test
 			if (args[0] == "--check")
-			{
 				CheckImplementation(args[1], args[2]);
-			}
 			else
 			{
-				string oldFile = args[0];
-				string newFile = args[1];
-				string patchFile = args[2];
+				var oldFile = args[0];
+				var newFile = args[1];
+				var patchFile = args[2];
 
 				try
 				{
-					using (FileStream output = new FileStream(patchFile, FileMode.Create))
+					using (var output = new FileStream(patchFile, FileMode.Create))
 						BinaryPatchUtility.Create(File.ReadAllBytes(oldFile), File.ReadAllBytes(newFile), output);
 				}
 				catch (FileNotFoundException ex)
@@ -48,58 +43,58 @@ namespace BsDiff
 			const string bsdiffPath = @"C:\Util\bsdiff.exe";
 			const string rbspatchPath = @"C:\Util\bspatch.exe";
 
-			string tempPath = Path.GetTempPath();
-			string referencePatchFileName = Path.Combine(tempPath, "reference.patch");
-			string portPatchFileName = Path.Combine(tempPath, "port.patch");
+			var tempPath = Path.GetTempPath();
+			var referencePatchFileName = Path.Combine(tempPath, "reference.patch");
+			var portPatchFileName = Path.Combine(tempPath, "port.patch");
 
 			// run reference implementation
-			Stopwatch referenceTime = Stopwatch.StartNew();
-			using (Process process = Process.Start(bsdiffPath, QuotePaths(oldFile, newFile, referencePatchFileName)))
+			var referenceTime = Stopwatch.StartNew();
+			using (var process = Process.Start(bsdiffPath, QuotePaths(oldFile, newFile, referencePatchFileName)))
 			{
 				process.WaitForExit();
 				referenceTime.Stop();
 			}
 
 			// run C# implementation
-			Stopwatch portTime = Stopwatch.StartNew();
-			using (FileStream output = new FileStream(portPatchFileName, FileMode.Create))
+			var portTime = Stopwatch.StartNew();
+			using (var output = new FileStream(portPatchFileName, FileMode.Create))
 				BinaryPatchUtility.Create(File.ReadAllBytes(oldFile), File.ReadAllBytes(newFile), output);
 			portTime.Stop();
 
 			Console.WriteLine("Patches created in {0} (reference) and {1} (C# port).", referenceTime.Elapsed, portTime.Elapsed);
 			Console.WriteLine("File sizes (in bytes) are {0:n0} (reference) and {1:n0} (C# port).", new FileInfo(referencePatchFileName).Length, new FileInfo(portPatchFileName).Length);
 
-			string[] outputFilePaths = new[] { "test-ref-ref.dat", "test-prt-ref.dat", "test-ref-prt.dat", "test-prt-prt.dat" }
+			var outputFilePaths = new[] { "test-ref-ref.dat", "test-prt-ref.dat", "test-ref-prt.dat", "test-prt-prt.dat" }
 				.Select(fn => Path.Combine(tempPath, fn))
 				.ToArray();
 
 			Console.Write("Applying reference patch with reference binary...");
-			using (Process process = Process.Start(rbspatchPath, QuotePaths(oldFile, outputFilePaths[0], referencePatchFileName)))
+			using (var process = Process.Start(rbspatchPath, QuotePaths(oldFile, outputFilePaths[0], referencePatchFileName)))
 				process.WaitForExit();
 			Console.WriteLine("done.");
 
 			Console.Write("Applying port patch with reference binary...");
-			using (Process process = Process.Start(rbspatchPath, QuotePaths(oldFile, outputFilePaths[1], portPatchFileName)))
+			using (var process = Process.Start(rbspatchPath, QuotePaths(oldFile, outputFilePaths[1], portPatchFileName)))
 				process.WaitForExit();
 			Console.WriteLine("done.");
 
 			Console.Write("Applying reference patch with port binary...");
-			using (FileStream input = new FileStream(oldFile, FileMode.Open, FileAccess.Read, FileShare.Read))
-			using (FileStream output = new FileStream(outputFilePaths[2], FileMode.Create))
+			using (var input = new FileStream(oldFile, FileMode.Open, FileAccess.Read, FileShare.Read))
+			using (var output = new FileStream(outputFilePaths[2], FileMode.Create))
 				BinaryPatchUtility.Apply(input, () => new FileStream(referencePatchFileName, FileMode.Open, FileAccess.Read, FileShare.Read), output);
 			Console.WriteLine("done.");
 
 			Console.Write("Applying port patch with port binary...");
-			using (FileStream input = new FileStream(oldFile, FileMode.Open, FileAccess.Read, FileShare.Read))
-			using (FileStream output = new FileStream(outputFilePaths[3], FileMode.Create))
+			using (var input = new FileStream(oldFile, FileMode.Open, FileAccess.Read, FileShare.Read))
+			using (var output = new FileStream(outputFilePaths[3], FileMode.Create))
 				BinaryPatchUtility.Apply(input, () => new FileStream(portPatchFileName, FileMode.Open, FileAccess.Read, FileShare.Read), output);
 			Console.WriteLine("done.");
 
-			int errors = 0;
-			byte[] expectedOutput = File.ReadAllBytes(newFile);
-			foreach (string filePath in outputFilePaths)
+			var errors = 0;
+			var expectedOutput = File.ReadAllBytes(newFile);
+			foreach (var filePath in outputFilePaths)
 			{
-				byte[] actualOutput = File.ReadAllBytes(filePath);
+				var actualOutput = File.ReadAllBytes(filePath);
 				if (!expectedOutput.SequenceEqual(actualOutput))
 				{
 					Console.Error.WriteLine("Incorrect results in {0}.", Path.GetFileName(filePath));
--- src/BsPatchTool/Program.cs
+++ src/BsPatchTool/Program.cs
@@ -1,35 +1,31 @@
-
-using System;
-using System.IO;
 using BsDiff;
 
-namespace BsPatch
+namespace BsPatchTool;
+
+class Program
 {
-	class Program
+	static void Main(string[] args)
 	{
-		static void Main(string[] args)
+		// check for correct usage
+		if (args.Length != 3)
 		{
-			// check for correct usage
-			if (args.Length != 3)
-			{
-				Console.Error.WriteLine("bsdiff oldfile newfile patchfile");
-				return;
-			}
+			Console.Error.WriteLine("bsdiff oldfile newfile patchfile");
+			return;
+		}
 
-			string oldFile = args[0];
-			string newFile = args[1];
-			string patchFile = args[2];
+		var oldFile = args[0];
+		var newFile = args[1];
+		var patchFile = args[2];
 
-			try
-			{
-				using (FileStream input = new FileStream(oldFile, FileMode.Open, FileAccess.Read, FileShare.Read))
-				using (FileStream output = new FileStream(newFile, FileMode.Create))
-					BinaryPatchUtility.Apply(input, () => new FileStream(patchFile, FileMode.Open, FileAccess.Read, FileShare.Read), output);
-			}
-			catch (FileNotFoundException ex)
-			{
-				Console.Error.WriteLine("Could not open '{0}'.", ex.FileName);
-			}
+		try
+		{
+			using (var input = new FileStream(oldFile, FileMode.Open, FileAccess.Read, FileShare.Read))
+			using (var output = new FileStream(newFile, FileMode.Create))
+				BinaryPatchUtility.Apply(input, () => new FileStream(patchFile, FileMode.Open, FileAccess.Read, FileShare.Read), output);
+		}
+		catch (FileNotFoundException ex)
+		{
+			Console.Error.WriteLine("Could not open '{0}'.", ex.FileName);
 		}
 	}
 }
